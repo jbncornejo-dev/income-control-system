09baac7c75497bac3d73b95ab7740c687242a8c5
aacm592 <pxe592@gmail.com>
2026-09-02 21:43:37 -0400

    feat: instalar y configurar entorno vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+import './bootstrap'
+import { createApp } from 'vue'
+
+const App = {
+  template: '<div><h2>Vue 3 está configurado y funcionando</h2></div>'
+}
+
+createApp(App).mount('#app')
