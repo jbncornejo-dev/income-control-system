7bddb8a66a8ceea620bb61ca866f4ac5c884bfbf
Nathalia <sunnchanneln@gmail.com>
2026-09-08 09:29:56 -0400

    usuarios creados para desarrollo

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -42,6 +42,7 @@ class HandleInertiaRequests extends Middleware
                     'id' => $request->user()->id,
                     'name' => $request->user()->name,
                     'email' => $request->user()->email,
+                    'rol' => $request->user()->rol?->nombre_rol,
                 ] : null,
             ],
             'flash' => [
