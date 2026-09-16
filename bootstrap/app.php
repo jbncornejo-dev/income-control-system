f3bd8355fca94ee612690ee8b876b914c5c54720
Joaquin Tapia <Joaquin.Tapia0724@jala.university>
2026-09-16 11:18:12 -0400

    fix: Configurar trustProxies y forzar HTTPS para Inertiai

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Confiar en los proxies de Render para mantener el protocolo HTTPS
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => EnsureUserHasRole::class,
         ]);
