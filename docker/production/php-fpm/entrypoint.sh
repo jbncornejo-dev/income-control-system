ff9d5273c330ccbf256d20aacee881294b3ab432
Joaquin Tapia <Joaquin.Tapia0724@jala.university>
2026-09-16 11:01:38 -0400

    Fix: Crear directorios de cache para Laravel

--- docker/production/php-fpm/entrypoint.sh
+++ docker/production/php-fpm/entrypoint.sh
@@ -2,31 +2,31 @@
 set -e
 
 # Initialize storage directory if empty
-# -----------------------------------------------------------
-# If the storage directory is empty, copy the initial contents
-# and set the correct permissions.
-# -----------------------------------------------------------
 if [ ! "$(ls -A /var/www/storage)" ]; then
   echo "Initializing storage directory..."
   cp -R /var/www/storage-init/. /var/www/storage
-  chown -R www-data:www-data /var/www/storage
 fi
+
+# 1. Crear las carpetas obligatorias de Laravel que Git ignora
+mkdir -p /var/www/storage/framework/views
+mkdir -p /var/www/storage/framework/cache/data
+mkdir -p /var/www/storage/framework/sessions
+mkdir -p /var/www/bootstrap/cache
+
+# 2. Dar permisos al usuario www-data (el que corre PHP y Apache)
+chown -R www-data:www-data /var/www/storage
+chown -R www-data:www-data /var/www/bootstrap/cache
 
 # Remove storage-init directory
 rm -rf /var/www/storage-init
 
 # Run Laravel migrations
-# -----------------------------------------------------------
-# Ensure the database schema is up to date.
-# -----------------------------------------------------------
 php artisan migrate --force
 
 # Clear and cache configurations
-# -----------------------------------------------------------
-# Improves performance by caching config and routes.
-# -----------------------------------------------------------
 php artisan config:cache
 php artisan route:cache
+php artisan view:cache
 
 # Run the default command
 exec "$@"
