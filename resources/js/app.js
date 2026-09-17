1b68a3335ac7a5d31689a31e341dfa36174dff64
aacm592 <pxe592@gmail.com>
2026-09-16 23:14:14 -0400

    fix: añadido al commit resources/js/app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,16 +7,17 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import ToastContainer from './components/ui/ToastContainer.vue'
 
 createInertiaApp({
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        const app = createApp({ render: () => h(App, props) })
-        const pinia = createPinia()
-        app.use(pinia)
-        app.use(plugin)
-        app.component('ToastContainer', ToastContainer)
-        app.mount(el)
-    },
-    progress: {
-        color: '#1E3A5F',
-    },
+  resolve: (name) =>
+    resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+  setup({ el, App, props, plugin }) {
+    const app = createApp({ render: () => h(App, props) })
+    const pinia = createPinia()
+    app.use(pinia)
+    app.use(plugin)
+    app.component('ToastContainer', ToastContainer)
+    app.mount(el)
+  },
+  progress: {
+    color: '#1E3A5F'
+  }
 })
